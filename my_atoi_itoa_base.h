b8d3901fa26b1938173755e79c52e2f69992ed51
Nicolas Pelletier <nicopelletier39@gmail.com>
2021-02-15 21:25:45 +0100

    evalexpr: my_atoi_itoa_base: header

---
+++ my_atoi_itoa_base.h
@@ -0,0 +1,10 @@
+#ifndef MY_ATOI_ITOA_BASE_H
+#define MY_ATOI_ITOA_BASE_H
+
+#include <stddef.h>
+
+int my_atoi_base(const char *str, const char *base);
+char *my_itoa_base(int i, const char *base);
+
+#endif /* MY_ATOI_ITOA_BASE_H */
+
