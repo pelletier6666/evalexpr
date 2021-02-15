b5a1a182467a8882629195bf4eb9ca728adf0fcb
Nicolas Pelletier <nicopelletier39@gmail.com>
2021-02-15 20:43:02 +0100

    evalexpr: my_readline: change name of one function to ensure consistency and clear-sightedness

--- my_readline.h
+++ my_readline.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 char *my_fgets(char *str, size_t len_to_read, FILE *stream);
-char *my_readline_with_stream(FILE *stream);
+char *my_readline_from_stream(FILE *stream);
 char *my_readline(void);
 
 #endif
