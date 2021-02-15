2e28f2cd93e51255575700e9396147320119242a
Nicolas Pelletier <nicopelletier39@gmail.com>
2021-02-15 20:49:28 +0100

    evalexpr: my_readline: 4 Unit tests that test my_readline func

--- tests/test-my_readline.c
+++ tests/test-my_readline.c
@@ -1,34 +1,56 @@
 #include <criterion/criterion.h>
+#include <string.h>
 #include "my_readline.h"
 
+FILE *generate_stream_from_string(char *string)
+{
+    FILE *stream = fopen("tmp_test-file", "w+");
+    fputs(string, stream);
+    fseek(stream, 0, SEEK_SET);
+    return stream;
+}
 Test(my_readline, line_with_several_jump_line)
 {
-    char *expected = "iiiiiiiiiiii\niiiiiiiiiiiiii\niiiiiiiiiiiiiiiii\niiiiiiiiiiiiiiiiiiiii\n";
-    size_t expected_size = strlen(expected);
-    FILE *stream = open_memstream(&expected, &expected_size);
-    if (stream == NULL)
-        cr_skip_test();
-    char *result = my_readline_with_stream(stream);
+    char *string = "iiiiiiiiiiii\nii";
+    FILE *stream = generate_stream_from_string(string);
+
+    char *result = my_readline_from_stream(stream);
+    char *expected = "iiiiiiiiiiii";
+
     cr_assert_str_eq(result, expected);
+    fclose(stream);
 }
 Test(my_readline, empty_line)
 {
+    char *string = "";
+    FILE *stream = generate_stream_from_string(string);
+
+    char *result = my_readline_from_stream(stream);
     char *expected = "";
-    size_t expected_size = strlen(expected);
-    FILE *stream = open_memstream(&expected, &expected_size);
-    if (stream == NULL)
-        cr_skip_test();
-    char *result = my_readline_with_stream(stream);
+
     cr_assert_str_eq(result, expected);
+    fclose(stream);
 }
 Test(my_readline, line_with_eof)
 {
-    char *expected = "hhhhhhhhhhhhhhhhhhhhhh";
-    size_t expected_size = strlen(expected);
-    FILE *stream = open_memstream(&expected, &expected_size);
-    if (stream == NULL)
-        cr_skip_test();
-    char *result = my_readline_with_stream(stream);
-    cr_assert_str_eq(result, expected);
+    char *string = "hhhhh\n";
+    FILE *stream = generate_stream_from_string(string);
 
+    char *result = my_readline_from_stream(stream);
+    char *expected = "hhhhh";
+
+    cr_assert_str_eq(result, expected);
+    fclose(stream);
+}
+
+Test(my_readline, line_with_eof1)
+{
+    char *string = "\nhhhh";
+    FILE *stream = generate_stream_from_string(string);
+
+    char *result = my_readline_from_stream(stream);
+    char *expected = "";
+
+    cr_assert_str_eq(result, expected);
+    fclose(stream);
 }
