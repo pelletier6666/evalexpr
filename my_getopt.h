68980754fb095c7fbd2393f08ff8de9f89ce8f5b
Nicolas Pelletier <nicopelletier39@gmail.com>
2021-03-06 17:12:43 +0100

    my_getopt.h: Refactor code by both dividing the main function in smaller units and include a new header

--- my_getopt.h
+++ my_getopt.h
@@ -1,25 +1,14 @@
 #ifndef MY_GETOPT_H
 #define MY_GETOPT_H
 
-#include <stddef.h>
-
-enum traversal_type 
-{
-    none,
-    prefix,
-    infix,
-    suffix
-};
-
-struct opt
-{
-    char *b;
-    char *o;
-    enum traversal_type p;
-    char l;
-    char h;
-};
+#include "my_getopt_utils.h"
 
 struct opt *my_getopt(int argc, char **argv, int *error);
+
+void set_option_field(struct opt *option, char opt_flag, char *opt_value);
+
+enum traversal_type get_traversal_type(char *opt_value);
+
+struct opt *build_opt(char *base, char *oper, enum traversal_type print_format, char lexer_flag, char help_flag);
 
 #endif /* MY_GETOPT_H */
